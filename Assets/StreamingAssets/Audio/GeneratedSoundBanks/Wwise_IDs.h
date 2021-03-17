e7b681a036430d6d192e565f3db172a2ef86c023
funduky <funduky@mail.ru>
2021-03-17 13:06:25 +0300

    sound design and music implemented

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,42 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID FIRE = 2678880713U;
+        static const AkUniqueID AMB_INSIDE = 1564366702U;
+        static const AkUniqueID AMB_OUTSIDE = 1013211851U;
+        static const AkUniqueID D_CONSTRACTION = 3218541849U;
+        static const AkUniqueID D_CROWD = 1589574917U;
+        static const AkUniqueID D_DOG = 4001482824U;
+        static const AkUniqueID D_DRUMMING = 3445241757U;
+        static const AkUniqueID D_FIGHT = 2021402380U;
+        static const AkUniqueID D_PHONE = 4204937310U;
+        static const AkUniqueID D_SCREAM = 3357276237U;
+        static const AkUniqueID D_SEX = 3628976722U;
+        static const AkUniqueID D_TV = 2305070748U;
+        static const AkUniqueID FOOT = 3031504797U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LAMP = 657744959U;
+        static const AkUniqueID MUSIC_START = 3725903807U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SURFACE
+        {
+            static const AkUniqueID GROUP = 1834394558U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace SURFACE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AZIMUTH = 1437246667U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -25,7 +59,15 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+        static const AkUniqueID REVERB_2 = 35548098U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
